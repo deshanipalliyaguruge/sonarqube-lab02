217c65f0f8fa20be109696e5c5a4f643a6a2c3bd
T.P.G.D. Prilakshana <deshanipalliyaguruge2000@gmail.com>
2026-01-28 11:36:18 +0530

    Fix quality issues

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,18 @@
 package com.example;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class App {
+
+    static Logger logger = Logger.getLogger(App.class.getName());
 
     public static void main(String[] args) throws Exception {
 
         Calculator calc = new Calculator();
 
-        System.out.println(calc.calculate(10, 5, "add"));
+        if (logger.isLoggable(Level.INFO)) {
+            logger.info(String.valueOf(calc.calculate(10, 5, "add")));
+        }
 
         UserService service = new UserService();
         service.findUser("admin");
